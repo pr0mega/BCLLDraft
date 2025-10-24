03b88db14659a006094d5262d81cc4d9d981f0b9
pr0mega <Ryan.Teepen@gmail.com>
2025-10-24 12:54:39 -0700

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: If your repo name is different than BCLL_Draft, change the base below to '/<YourRepoName>/'
 export default defineConfig({
   plugins: [react()],
-  base: '/BCLL_Draft/',
+  base: '/BCLLDraft/',
 })
